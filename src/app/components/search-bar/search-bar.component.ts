379a9b7078d60e60c718aaf985da54f852f56066
Tóth Bence <tothbence0531@gmail.com>
2025-03-26 12:02:11 +0100

    searchbar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../modules/material.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [MaterialModule],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
-export class SearchBarComponent {}
+export class SearchBarComponent {
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      location: [''],
+      type: [''],
+      budget: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.searchForm.value);
+  }
+}
