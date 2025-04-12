60b5e08882d15031171f002052b2497a55a66770
Tóth Bence <tothbence0531@gmail.com>
2025-04-12 19:51:51 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   themeService = inject(ThemeService);
+  title = 'ingatlan-kereso';
 
   ngOnInit(): void {
     const storageTheme = localStorage.getItem('theme');
